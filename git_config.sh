3df958850ecaca5fc014d48ea80578e0f2bf6949
LukasJurena <jurenalukash@gmail.com>
2024-09-23 08:27:47 +0200

    hello_world

---
+++ git_config.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git config --global user.name "LukasJurena" 
+git config --global user.email "jurenalukash@gmail.com"
+echo "Přihlášení uživatele proběhlo v pořádku"
+#cd eshop_app/
+#"https://fonts.bunny.net"
+#fontshare
