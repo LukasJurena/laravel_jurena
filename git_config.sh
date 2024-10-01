804feb7a9c3d567ddc618956512436e6877222b8
LukasJurena <jurenalukash@gmail.com>
2024-10-01 08:37:29 +0200

    migrate zmeny a git zmena

--- git_config.sh
+++ git_config.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+git config --list
+git config --global --unset user.name
+git config --global --unset user.email
 git config --global user.name "LukasJurena" 
 git config --global user.email "jurenalukash@gmail.com"
-echo "Přihlášení uživatele proběhlo v pořádku"
+echo "-------------------------------------------------------------------------------Přihlášení uživatele proběhlo v pořádku------------------------------------------------------------------------------"
+git config --list
 #cd eshop_app/
 #"https://fonts.bunny.net"
 #fontshare
